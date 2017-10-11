543e29251c20df76d90f2f4c40e3f5f5118977df
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2017-10-11 13:20:08 +0800

    Import original EXM 3.4.1 code

---
+++ src/Sitecore.Support.144608/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.144608")]
+[assembly: AssemblyProduct("Sitecore.Support.144608")]
+[assembly: ComVisible(false)]
